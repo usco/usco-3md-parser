f9e36efbc6963ed04f2c92cb2416f02ddaa925cf
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-16 14:20:25 +0100

    feat(node.js support): added wrapper around parser function to enable node.js support - returns a duplex stream wrapping the parser function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import detectEnv from 'composite-detect'
 import workerSpawner from './workerSpawner'
-//import makeStreamParser from './parseStream'
-//import through2 from 'through2'
+// import makeStreamParser from './parseStream'
+import through2 from 'through2'
 import makeStreamParser from './parse'
 
 /**
@@ -15,6 +15,30 @@ import makeStreamParser from './parse'
  *
 **/
 
+function wrapper () {
+  const Duplex = require('stream').Duplex
+  class Outer extends Duplex {
+    constructor () {
+      super({readableObjectMode: true})
+      this.streamParser = makeStreamParser(data => this.push(data))
+      this.on('finish', () => this.streamParser.end())
+    }
+
+    _read (size) {}
+
+    _write (chunk, encoding, callback) {
+      this.streamParser.write(Buffer(chunk))
+      callback()
+    }
+  }
+  return new Outer()
+}
+/**
+ * parses and return a stream of parsed 3mf data
+ * @param {Object} parameters parameters for the parser
+ * @param {Boolean} parameters.useWorker use web workers (browser only) defaults to true in browser
+ * @return {Object} stream of parsed 3mf data in the form {}
+ */
 export default function makeParsedStream (parameters = {}) {
   const defaults = {
     useWorker: (detectEnv.isBrowser === true)
@@ -22,6 +46,5 @@ export default function makeParsedStream (parameters = {}) {
   parameters = Object.assign({}, defaults, parameters)
   const {useWorker} = parameters
 
-  console.log('using worker?', useWorker)
-  return useWorker ? workerSpawner() : makeStreamParser()
+  return useWorker ? workerSpawner() : wrapper()
 }
